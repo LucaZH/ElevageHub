a121f809e3df43eb9f339325928b4d05dc7cf413
LucaZH <lucarandria1@gmail.com>
2023-05-07 16:54:59 +0300

    update model

--- elevagehub_app/models.py
+++ elevagehub_app/models.py
@@ -5,11 +5,10 @@ from django.contrib.auth.models import User
 class Animal(models.Model):
     nom = models.CharField(max_length=100)
     espece = models.CharField(max_length=100)
-    date_naissance = models.DateField()
-    date_achat = models.DateField()
-    poids_initial = models.DecimalField(max_digits=5, decimal_places=2)
+    date = models.DateField()
+    poids_initial = models.IntegerField()
     notes = models.TextField(blank=True, null=True)
-    photo = models.ImageField(upload_to='images/animal_photos/', blank=True, null=True)
+    photo = models.ImageField(upload_to='media/images/animal_photos/', blank=True, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
@@ -19,14 +18,14 @@ class Activite(models.Model):
     type_activite = models.CharField(max_length=100)
     date_activite = models.DateTimeField()
     notes = models.TextField(blank=True, null=True)
-    photo = models.ImageField(upload_to='images/activite_photos/', blank=True, null=True)
+    photo = models.ImageField(upload_to='media/images/activite_photos/', blank=True, null=True)
 
 
 class Aliment(models.Model):
     nom = models.CharField(max_length=100)
     type_aliment = models.CharField(max_length=100)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    photo = models.ImageField(upload_to='images/activite_photos/', blank=True, null=True)
+    photo = models.ImageField(upload_to='media/images/activite_photos/', blank=True, null=True)
 
 
 class Stock(models.Model):
@@ -41,11 +40,3 @@ class Croissance(models.Model):
     animal = models.ForeignKey(Animal, on_delete=models.CASCADE)
     poids = models.DecimalField(max_digits=5, decimal_places=2)
     date_mesure = models.DateTimeField()
-
-
-class Alerte(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    animal = models.ForeignKey(Animal, on_delete=models.CASCADE)
-    type_alerte = models.CharField(max_length=100)
-    message = models.TextField()
-    date_alerte = models.DateTimeField()
